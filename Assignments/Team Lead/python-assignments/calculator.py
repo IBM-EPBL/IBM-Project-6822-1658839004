6ef190bc5ba9d350b0ab4f918e88556111746bcc
ALEX S <56356099+adminALEX@users.noreply.github.com>
2022-09-21 10:55:42 +0530

    String operations and Basic calculator in python

---
+++ Assignments/Team Lead/python-assignments/calculator.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 21 10:10:03 2022
+
+@author: alex
+"""
+
+# Calculator program in python
+
+#Calculator function
+def calculate(a, b, c):
+    if(c == '+'):
+        return a+b
+    elif(c=='-'):
+        return a-b
+    elif(c=='*'):
+        return a*b
+    elif(c=='/'):
+        return a//b
+    else:
+        return null
+
+#Main program
+first = int(input("Enter first number : "))     #Getting first number
+second = int(input("Enter second number : "))   #Getting second number
+
+operator = input("Enter the operation : ")[0]
+
+print("Your result is " , calculate(first,second,operator))
